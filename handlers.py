6ce44bd60935312681c0ae04fba1d2f67eddda4a
Sbrodov Egor <egor-ulstu@yandex.ru>
2019-08-13 14:56:59 +0300

    New handler added

--- handlers.py
+++ handlers.py
@@ -1,9 +1,19 @@
 from aiogram import types
+from aiogram.dispatcher import FSMContext
+
 from main import dp
 from dialogs import MainDialog
 
 
 @dp.message_handler(commands='start', state='*')
 async def start(message: types.Message):
-    await message.reply('Привет! Как тебя зовут?')
+    await message.answer('Привет! Как тебя зовут?')
     await MainDialog.enter_name.set()
+
+
+@dp.message_handler(state=MainDialog.enter_name)
+async def enter_name(message: types.Message, state: FSMContext):
+    async with state.proxy() as storage:
+        storage['name'] = message.text
+    await message.answer(f'Приятно познакомиться,  {message.text}. А сколько тебе лет?')
+    await MainDialog.enter_age.set()
