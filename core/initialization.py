d9de7c583f9fbdfa6b79a5b41b6a34555f8f4872
Sbrodov Egor <egor-ulstu@yandex.ru>
2019-08-16 12:09:25 +0300

    Redis storage connected

--- core/initialization.py
+++ core/initialization.py
@@ -2,7 +2,7 @@ import json
 import logging
 
 from aiogram import Bot, Dispatcher, executor
-from aiogram.contrib.fsm_storage.memory import MemoryStorage
+from aiogram.contrib.fsm_storage.redis import RedisStorage2
 from aiogram.contrib.middlewares.logging import LoggingMiddleware
 
 from core.quiz import Question
@@ -11,7 +11,7 @@ logging.basicConfig(level=logging.DEBUG)
 
 TOKEN = "617318489:AAGuwWvdS8KG5YUSoq9Us5s6FHOPOMmmdys"
 bot = Bot(TOKEN)
-dp = Dispatcher(bot, storage=MemoryStorage())
+dp = Dispatcher(bot, storage=RedisStorage2(host='localhost', port=6379))
 dp.middleware.setup(LoggingMiddleware())
 
 questions = []
